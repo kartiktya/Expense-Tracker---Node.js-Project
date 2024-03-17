00844f65e092ae52daa068a6ff90e34a9c2b95a4
kartiktya <kartiktyagi953@gmail.com>
2024-03-17 17:26:17 +0530

    Lets go live

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const fs = require('fs'); // node file system, which is a core module
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 var cors = require('cors');
 
@@ -13,13 +18,21 @@ const ExpenseFilesDownloaded = require('./models/ExpenseFilesDownloaded.js');
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'access.log'), 
+    { flags: 'a' }
+);
+
 app.use(cors());
+app.use(helmet());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 const userRoutes = require('./routes/user.js');
 const expenseRoutes = require('./routes/expense.js');
 const purchaseRoutes = require('./routes/purchase.js');
 const premiumRoutes = require('./routes/premium.js');
 const passwordRoutes = require('./routes/password.js');
+
 
 app.use(bodyParser.json({ extended: false }));
 
@@ -53,4 +66,4 @@ sequelize
 .then()
 .catch(err => console.log(err));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
